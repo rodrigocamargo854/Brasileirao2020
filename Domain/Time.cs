86a0f259efd8e4120ca4706fbed9a415318a1926
rodrigocamargo <rodrigocamargo854@gmail.com>
2020-10-16 18:14:45 -0300

    Adicionados na classe Time metodos addPontos, addVitorias,AddEmpates,AddArtilheiros

--- Domain/Time.cs
+++ Domain/Time.cs
@@ -6,11 +6,14 @@ namespace Brasileirao2020
     {
         public string Nome { get; set; }
         public List<Jogador> Jogadores { get; set; }
-
-        public Time(string nome)
-        {
-            Nome = nome;
-        }
+        public int PontosTime { get; set; }
+        public int Vitorias { get; set; }
+        public int Derrotas { get; set; }
+        public int Empates { get; set; }
+        public double PorcentagemDeAproveitamento { get; set; }
+        public int GolsPro { get; set; }
+        public int GolsContra { get; set; }
+        
 
         public void AddJogador()
         {
