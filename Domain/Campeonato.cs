73cebc0384a95e39835cd5f9a0a0bd7cef905a52
rodrigocamargo <rodrigocamargo854@gmail.com>
2020-10-18 16:18:09 -0300

    Adicionado test do metodo GeraConflitos da classe campeonatos

--- Domain/Campeonato.cs
+++ Domain/Campeonato.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Domain
 {
+
+
     public class Campeonato
     {
         public List<Time> Times { get; set; } = new List<Time>();//inicializa vazia
@@ -10,6 +13,22 @@ namespace Domain
         public int Rodadas { get; set; }
 
         public string[] Conflitos { get; set; }
+
+
+        private void embaralhar(List<Time> times)
+        {
+            // cria um objeto da classe Random
+            Random rnd = new Random();
+
+            // vamos embaralhar o ArrayList
+            for (int i = 0; i < times.Count; i++)
+            {
+                int a = rnd.Next(times.Count);
+                var temp = times[i];
+                times[i] = times[a];
+                times[a] = temp;
+            }
+        }
 
 
         ///Regra de negocio usuario CBF
@@ -32,33 +51,38 @@ namespace Domain
         public Time[,] GeraConflitos(List<Time> times, Usuario usuario)
         {
 
+            embaralhar(times);
 
-            Random mistura = new Random();
             Time[,] tabelaConflitos = new Time[4, 2];
 
             //recebe a conversão times em array
             Time[] arrayTimes = times.ToArray();
 
+
+
             if (usuario is Cbf)
             {
                 //objeto do tipo Random para misturar os times
 
+                int s = -1;
 
-                for (int i = 0; i < arrayTimes.Length; i++)
+                for (int i = 0; i < arrayTimes.Length/2; i+=2)
                 {
+                    
+
                     for (int j = 0; j < 2; j++)
                     {
-                        tabelaConflitos[i, j] = arrayTimes[i];
+                        
+                        tabelaConflitos[i,j] = arrayTimes[++s];
+
                     }
-                    return tabelaConflitos;
                 }
+                return tabelaConflitos;
+
             }
-
             return tabelaConflitos = null;
-
-
         }
-
+        // método que embaralha os jogadores da lista
 
     }
 
